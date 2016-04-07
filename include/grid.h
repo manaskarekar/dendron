90335490f5899b1771ffef09c3a2312bd51d621b
Manas Karekar <powerslave12r@gmail.com>
2016-04-07 13:34:50 -0400

    Adding member functions to the Grid class.

--- include/grid.h
+++ include/grid.h
@@ -8,20 +8,29 @@ class Grid {
 	std::vector < std::vector <int> > grid{}, grid_next{};
 	size_t grid_size{};
 
-	const int dead = 0;
-	const int alive = 1;
+	const int cell_dead = 0;
+	const int cell_alive = 1;
+	const int cell_invalid = -1;
 
 	void set_grid(std::vector < std::vector <int> > grid, int value);
 	void set_cell_dead(std::vector < std::vector <int> > grid, int i, int j);
 	void set_cell_alive(std::vector < std::vector <int> > grid, int i, int j);
 
+	int get_cell_state(int i, int j);
+	int count_neighbors(int i, int j);
+
 public:
 	Grid()=default;
 	Grid(const int n) : grid_size(n) {
-		set_grid(grid, dead);
-		set_grid(grid_next, dead);
+		set_grid(grid, cell_dead);
+		set_grid(grid_next, cell_dead);
 	};
 
+	void seed_grid(std::vector < std::vector <int> > grid, std::vector< std::tuple <int, int> > alive_cells);
+	void update_grid();
+	void process_grid();
+	void erase_grid();
+	void draw_grid();
 };
 
 void Grid::set_grid(std::vector < std::vector <int> > grid_, int value){ // underscore after "grid" to avoid any name clashes, shouldn't happen, but I'll remove it once I've confirmed it.
@@ -33,10 +42,36 @@ void Grid::set_grid(std::vector < std::vector <int> > grid_, int value){ // unde
 }
 
 void Grid::set_cell_dead(std::vector < std::vector <int> > grid_, int i, int j){
-	grid_[i][j] = dead;
+	grid_[i][j] = cell_dead;
 }
 
 void Grid::set_cell_alive(std::vector < std::vector <int> > grid_, int i, int j){
-	grid_[i][j] = alive;
+	grid_[i][j] = cell_alive;
 }
 
+int Grid::get_cell_state(int i, int j){
+	if (i < 0 || j < 0 || i >= grid_size || j >= grid_size){
+		return cell_invalid; //
+	}
+	return grid[i][j];
+}
+
+int Grid::count_neighbors(int i, int j){
+	int count = 0;
+
+	//horizontal
+	if (get_cell_state(i-1, j) == cell_alive) count++;
+	if (get_cell_state(i+1, j) == cell_alive) count++;
+	//vertical
+	if (get_cell_state(i, j-1) == cell_alive) count++;
+	if (get_cell_state(i, j+1) == cell_alive) count++;
+	//diagonal
+	if (get_cell_state(i-1, j-1) == cell_alive) count++;
+	if (get_cell_state(i+1, j-1) == cell_alive) count++;
+	if (get_cell_state(i-1, j+1) == cell_alive) count++;
+	if (get_cell_state(i+1, j+1) == cell_alive) count++;
+	return count;
+}
+
+
+void Grid::seed_grid(std::vector < std::vector <int> > grid, std::vector< std::tuple <int, int> > alive_cells)
