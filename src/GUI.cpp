0c7254adb6f7260c28d85eea619bfe5777798473
Manas Karekar <powerslave12r@gmail.com>
2016-04-08 08:32:03 -0400

    Playing around with SFML. Will probably move src/GUI.cpp into scratch.

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -5,7 +5,22 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
+	sf::Color gray(128,128,128);
+	sf::RectangleShape dead_cell, live_cell;
+	sf::Vector2f cell_size(16,16);
+
 	sf::RenderWindow window(sf::VideoMode(640, 480), "Dendron");
+	window.clear(gray);
+
+	dead_cell.setSize(cell_size);
+	dead_cell.setFillColor(sf::Color(255, 255, 255));
+	window.draw(dead_cell);
+
+	live_cell.setSize(cell_size);
+	live_cell.setFillColor(sf::Color(0, 0, 0));
+	window.draw(live_cell);
+
+	window.display();
 
 	Grid g{25};
 	std::vector< std::vector <int> > seed_cells {
@@ -32,7 +47,7 @@ int main(){
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		window.clear(sf::Color::Black);
+		window.clear(gray);
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 		g.erase_grid();
 		g.update_grid(); //TODO: Pass around functions, like the python version
