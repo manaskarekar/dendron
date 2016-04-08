980af331cb73131523f773fc263f714011f1d2aa
Manas Karekar <powerslave12r@gmail.com>
2016-04-07 22:31:41 -0400

    Have a basic, inefficient version of Conway's Game Of Life. Lots of improvements to be done.

--- include/grid.h
+++ include/grid.h
@@ -3,14 +3,20 @@
 
 #include <iostream>
 #include <vector>
+#include <cstdlib> //Used for system, screen clear. TODO: Remove this once using proper GUI.
 
 class Grid {
 	std::vector < std::vector <int> > grid{}, grid_next{};
 	int grid_size{};
 
-	const int cell_dead = 0;
-	const int cell_alive = 1;
-	const int cell_invalid = -1;
+	//const int cell_dead = 0;
+	//const int cell_alive = 1;
+	//const int cell_invalid = -1;
+
+	const char cell_dead = '.';
+	const char cell_alive = '#';
+	const char cell_invalid = 'x';
+
 
 	void set_grid(std::vector < std::vector <int> >& grid, int value);
 	void set_cell_dead(std::vector < std::vector <int> >& grid, int i, int j);
@@ -35,8 +41,8 @@ public:
 
 void Grid::set_grid(std::vector < std::vector <int> >& grid_, int value){ // underscore after "grid" to avoid any name clashes, shouldn't happen, but I'll remove it once I've confirmed it.
 	std::vector <int> row{};
-	for (int i=0; i < grid_size; i++){
-		for (int j=0; j < grid_size; j++){
+	for(int i=0; i < grid_size; i++){
+		for(int j=0; j < grid_size; j++){
 			row.push_back(value);
 		}
 		grid_.push_back(row);
@@ -82,14 +88,38 @@ void Grid::seed_grid(std::vector< std::vector <int> > alive_cells){
 	}
 }
 
-void Grid::update_grid(){}
-void Grid::process_grid(){}
-void Grid::erase_grid(){}
+void Grid::update_grid(){
+	set_grid(grid_next, cell_dead); //TODO: Instead of setting the next grid as dead, copy the current grid and modify based on previous states.
+	process_grid();
+	grid = grid_next; //TODO: check 2 things: assignment and scope of these
+
+}
+
+void Grid::process_grid(){
+	for(int i=0; i < grid_size; i++){
+		for(int j=0; j < grid_size; j++){
+			int count = count_neighbors(i, j);
+			if (count != 2 && count != 3) {
+				set_cell_dead(grid_next, i, j);
+			} else if (count == 2) {
+				if (get_cell_state(i, j) == cell_alive){
+					set_cell_alive(grid_next, i, j);
+				}
+			} else if (count == 3) {
+				set_cell_alive(grid_next, i, j);
+			}
+		}
+	}
+}
+
+void Grid::erase_grid(){
+	std::system("clear");
+}
 
 void Grid::draw_grid(){
-	for (int i=0; i < grid_size; i++){
-		for (int j=0; j < grid_size; j++){
-			std::cout << get_cell_state(i, j) << " ";
+	for(int i=0; i < grid_size; i++){
+		for(int j=0; j < grid_size; j++){
+			std::cout << static_cast<char>(get_cell_state(i, j)) << " ";
 		}
 		std::cout << std::endl;
 	}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "../include/grid.h"
 
 int main(){
-	Grid g{10};
+	Grid g{45};
 	std::vector< std::vector <int> > seed_cells {
-	{1,1}, {2,2}, {3,3}, {4,4}
+		{1,1}, {2,2}, {3,3}, {4,4}
 	};
 
-	g.seed_grid(seed_cells);
+	std::vector< std::vector <int> > seed_block {
+		{1,1}, {1,2}, {2,1}, {2,2}
+	};
+
+
+	std::vector< std::vector <int> > seed_pentonimo {
+		{20,19}, {21,19}, {21,18}, {20,20}, {22,19}
+	};
+
+	g.erase_grid();
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	g.seed_grid(seed_pentonimo);
 	g.draw_grid();
+
+	while(1){
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		g.erase_grid();
+		g.update_grid(); //TODO: Pass around functions, like the python version
+		g.draw_grid();
+	}
 	return 0;
 }
