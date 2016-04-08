18420caba936bea659f84294bc0c9c7a6b13a520
Manas Karekar <powerslave12r@gmail.com>
2016-04-07 21:28:12 -0400

    Have the basic grid being drawn and seeded successfully.

--- include/grid.h
+++ include/grid.h
@@ -6,46 +6,48 @@
 
 class Grid {
 	std::vector < std::vector <int> > grid{}, grid_next{};
-	size_t grid_size{};
+	int grid_size{};
 
 	const int cell_dead = 0;
 	const int cell_alive = 1;
 	const int cell_invalid = -1;
 
-	void set_grid(std::vector < std::vector <int> > grid, int value);
-	void set_cell_dead(std::vector < std::vector <int> > grid, int i, int j);
-	void set_cell_alive(std::vector < std::vector <int> > grid, int i, int j);
+	void set_grid(std::vector < std::vector <int> >& grid, int value);
+	void set_cell_dead(std::vector < std::vector <int> >& grid, int i, int j);
+	void set_cell_alive(std::vector < std::vector <int> >& grid, int i, int j);
 
 	int get_cell_state(int i, int j);
 	int count_neighbors(int i, int j);
 
 public:
-	Grid()=default;
+	//Grid()=default;
 	Grid(const int n) : grid_size(n) {
 		set_grid(grid, cell_dead);
 		set_grid(grid_next, cell_dead);
 	};
 
-	void seed_grid(std::vector < std::vector <int> > grid, std::vector< std::tuple <int, int> > alive_cells);
+	void seed_grid(std::vector< std::vector <int> > alive_cells);
 	void update_grid();
 	void process_grid();
 	void erase_grid();
 	void draw_grid();
 };
 
-void Grid::set_grid(std::vector < std::vector <int> > grid_, int value){ // underscore after "grid" to avoid any name clashes, shouldn't happen, but I'll remove it once I've confirmed it.
-	for (auto i=0; i < grid_size; i++){
-		for (auto j=0; j < grid_size; j++){
-			grid_[i][j] = value;
+void Grid::set_grid(std::vector < std::vector <int> >& grid_, int value){ // underscore after "grid" to avoid any name clashes, shouldn't happen, but I'll remove it once I've confirmed it.
+	std::vector <int> row{};
+	for (int i=0; i < grid_size; i++){
+		for (int j=0; j < grid_size; j++){
+			row.push_back(value);
 		}
+		grid_.push_back(row);
 	}
 }
 
-void Grid::set_cell_dead(std::vector < std::vector <int> > grid_, int i, int j){
+void Grid::set_cell_dead(std::vector < std::vector <int> >& grid_, int i, int j){
 	grid_[i][j] = cell_dead;
 }
 
-void Grid::set_cell_alive(std::vector < std::vector <int> > grid_, int i, int j){
+void Grid::set_cell_alive(std::vector < std::vector <int> >& grid_, int i, int j){
 	grid_[i][j] = cell_alive;
 }
 
@@ -74,4 +76,23 @@ int Grid::count_neighbors(int i, int j){
 }
 
 
-void Grid::seed_grid(std::vector < std::vector <int> > grid, std::vector< std::tuple <int, int> > alive_cells)
+void Grid::seed_grid(std::vector< std::vector <int> > alive_cells){
+	for (auto c : alive_cells){
+		set_cell_alive(grid, c[0], c[1]);
+	}
+}
+
+void Grid::update_grid(){}
+void Grid::process_grid(){}
+void Grid::erase_grid(){}
+
+void Grid::draw_grid(){
+	for (int i=0; i < grid_size; i++){
+		for (int j=0; j < grid_size; j++){
+			std::cout << get_cell_state(i, j) << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-#include <vector>
+#include "../include/grid.h"
 
-class Grid {
+int main(){
+	Grid g{10};
+	std::vector< std::vector <int> > seed_cells {
+	{1,1}, {2,2}, {3,3}, {4,4}
+	};
 
-
-
-};
+	g.seed_grid(seed_cells);
+	g.draw_grid();
+	return 0;
+}
