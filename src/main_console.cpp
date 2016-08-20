7559d4ced9875e3d49aae60a4594a07fc0147b15
Manas Karekar <powerslave12r@gmail.com>
2016-08-20 15:18:45 -0400

    Renamed the terminal based version to main_console.cpp and the gui version to main.cpp

---
+++ src/main_console.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "../include/grid.h"
+
+// This is the controller for the linux terminal based version.
+
+int main(){
+	Grid g{45};
+	std::vector< std::vector <int> > seed_cells {
+		{1,1}, {2,2}, {3,3}, {4,4}
+	};
+
+	std::vector< std::vector <int> > seed_block {
+		{1,1}, {1,2}, {2,1}, {2,2}
+	};
+
+
+	std::vector< std::vector <int> > seed_pentonimo {
+		{20,19}, {21,19}, {21,18}, {20,20}, {22,19}
+	};
+
+	g.erase_grid();
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	g.seed_grid(seed_pentonimo);
+	g.draw_grid();
+
+	while(1){
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		g.erase_grid();
+		g.update_grid(); //TODO: Pass around functions, like the python version
+		g.draw_grid();
+	}
+	return 0;
+}
